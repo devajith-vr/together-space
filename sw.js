0747a2c359d7c07f9c4378f0726a0610b20e5b6c
Devajith VR <devajithvr25@gmail.com>
2026-06-03 12:55:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,14 @@
-const CACHE_NAME = 'together-v1';
-const urlsToCache = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './favicon.png'
-];
+const CACHE_NAME = 'together-v2';
+const STATIC = ['./index.html', './style.css', './app.js', './manifest.json', './favicon.svg'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(STATIC)).then(() => self.skipWaiting()));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
